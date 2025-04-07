5c6bad0fb52b6774e49abd810c06c53b3757e960
Dawn Ruby Nishimoto <dawn@pamaxie.com>
2025-04-06 17:34:22 -0700

    Initializes engine with Vulkan and Winit
    Sets up the basic structure for the engine, including Vulkan calling, window creation and management using Winit.
    This commit lays the groundwork for adding rendering and introduces basic debug functionality to allow monitoring performance.
    Includes a default AppData struct to handle Vulkan layer debugging and associated properties.
    You will need the [Vulkan SDK](https://vulkan.lunarg.com) to use and develop for this. Please download and install it for your platform of choice.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # JAREngine
-This is JAREngine (Jet another Rust Engine). The idea is to help with certain calls in Vulkan Libraries and build this out for functional game development via various features.
-The intention is to have a minimal, cross platform functional game engine that allows developers to not think about the complexities of Vulkan calls and simplify a lot of their functionality.
+This is JAREngine (Jet another Rust Engine). The idea is to help with certain calls in Vulkan Libraries and build this 
+out for functional game development via various features. The intention is to have a minimal, cross platform functional 
+game engine that allows developers to not think about the complexities of Vulkan calls and simplify a lot of their 
+functionality.
+
+You will need the [Vulkan SDK](https://vulkan.lunarg.com) to use this. Please download and install it for your platform
+of choice.
---
+++ src/main.rs
@@ -0,0 +1,209 @@
+#![allow(
+     dead_code,
+     unused_variables,
+     clippy::too_many_arguments,
+     clippy::unnecessary_wraps
+)]
+
+use std::collections::HashSet;
+use std::ffi::CStr;
+use anyhow::{anyhow, Result};
+use log::{debug, error, info, trace, warn};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::Instance;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::raw_window_handle::HasRawWindowHandle;
+use winit::window::{{Window, WindowBuilder}};
+
+//Adding this for Apple support x.x
+use vulkanalia::Version;
+use vulkanalia::vk::ExtDebugUtilsExtension;
+
+const PORTABILITY_MACOS_VERSION: Version = Version { major: 1, minor: 3, patch: 216 };
+
+
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+const VALIDATION_LAYER: vk::ExtensionName = vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+
+fn main() -> Result<()> {
+     pretty_env_logger::init();
+
+     // Create a Window to draw our stuff into
+     let event_loop = EventLoop::new()?;
+     let window = WindowBuilder::new()
+         .with_title("")
+         .with_inner_size(LogicalSize::new(800, 600))
+         .build(&event_loop)?;
+     let mut app = unsafe { App::create(&window)? };
+
+     event_loop.run(move |event, elwt| {
+          match event {
+               Event::AboutToWait => window.request_redraw(),
+               Event::WindowEvent { event, ..} => match event{
+                    //Redraw Request Event Handling
+                    WindowEvent::RedrawRequested =>
+                         if !elwt.exiting() {
+                              unsafe { app.render(&window) }.unwrap()
+                         }
+
+                    //Close Request Event Handling
+                    WindowEvent::CloseRequested => {
+                        elwt.exit();
+                        unsafe { app.destroy(); }
+                    }
+
+                    //Handle all other events that we haven't handled ourselves by just discarding them for now
+                    _ => {}
+               }
+               _ => {}
+          }
+     })?;
+
+     Ok(())
+}
+
+
+
+///Make this more customizable via JSON configs or via external code :)
+unsafe fn create_instance(
+     window: &Window,
+     entry: &Entry,
+     data: &mut AppData
+) -> Result<Instance> {
+     let application_info = vk::ApplicationInfo::builder()
+         .application_name(b"My Game\0")
+         .application_version(vk::make_version(1, 1, 0))
+         .engine_name(b"RAGEngine\0")
+         .engine_version(vk::make_version(1, 1, 0))
+         .api_version(vk::make_version(1, 4, 309));
+
+     let available_layers = entry
+         .enumerate_instance_layer_properties()?
+         .iter()
+         .map(|l| l.layer_name)
+         .collect::<HashSet<_>>();
+
+     if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
+          return Err(anyhow!("Validation layers are not supported."));
+     }
+
+     let layers =
+         if VALIDATION_ENABLED { vec![VALIDATION_LAYER.as_ptr()] }
+         else{ Vec::new() };
+
+     let mut extensions = vk_window::get_required_instance_extensions(window)
+         .iter()
+         .map(|e| e.as_ptr())
+         .collect::<Vec<_>>();
+
+     let flags =
+         if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION
+         {
+              info!("Enabling macOS portability extensions");
+              extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+              extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+              vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+         }
+         else{
+              vk::InstanceCreateFlags::empty()
+         };
+
+     if VALIDATION_ENABLED {
+          extensions.push(vk::EXT_DEBUG_UTILS_EXTENSION.name.as_ptr());
+     }
+
+     let mut info = vk::InstanceCreateInfo::builder()
+         .application_info(&application_info)
+         .enabled_layer_names(&layers)
+         .enabled_extension_names(&extensions)
+         .flags(flags);
+
+     let extensions = vk_window::get_required_instance_extensions(window)
+         .iter()
+         .map(|e| e.as_ptr())
+         .collect::<Vec<_>>();
+
+     let mut debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+         .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+         .message_type(
+              vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                  | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                  | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+         )
+         .user_callback(Some(debug_callback));
+
+     if VALIDATION_ENABLED {
+          info = info.push_next(&mut debug_info);
+     }
+
+     let instance = entry.create_instance(&info, None)?;
+     Ok(instance)
+}
+
+///Does the Debug Callback handling. Long term we want to push this to an API endpoint so we can
+/// publish it locally via out application that handles all engine components.
+extern "system" fn debug_callback(
+     severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+     type_: vk::DebugUtilsMessageTypeFlagsEXT,
+     data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+     //user_data: *mut std::os::raw::c_void,
+     _: *mut std::ffi::c_void,) -> vk::Bool32 {
+     let data = unsafe { *data };
+     let message = unsafe { CStr::from_ptr(data.message) };
+
+     if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+          error!("{:?}", message);
+     } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING{
+          warn!("{:?}", message);
+     } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO{
+          debug!("{:?}", message);
+     }else{
+          trace!("{:?}", message);
+     }
+
+     vk::FALSE
+}
+
+
+///Literally our application (the Window and other things that we draw in it :)
+#[derive(Clone, Debug)]
+struct App {
+     entry: Entry,
+     instance: Instance,
+     data: AppData,
+}
+
+impl App {
+     ///Creates our app
+     unsafe fn create(window: &Window) -> Result<Self> {
+          let loader = LibloadingLoader::new(LIBRARY)?;
+          let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+          let mut data = AppData::default();
+          let instance = create_instance(window, &entry, &mut data)?;
+          Ok(Self { entry, instance, data })
+     }
+
+     ///Renders a frame for our app
+     unsafe fn render(&mut self, window: &Window) -> Result<()>{
+          Ok(())
+     }
+
+     ///Kills our App
+     unsafe fn destroy(&mut self) {
+          if VALIDATION_ENABLED{
+               self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
+          }
+
+          self.instance.destroy_instance(None);
+     }
+}
+
+/// Handle the Vulkan Associated Properties (Vulkan should do this for us ig)
+#[derive(Clone, Debug, Default)]
+struct AppData{
+     messenger: vk::DebugUtilsMessengerEXT,
+}
